5e215861125da31c672417f6ef4190db8bba1f05
Sergey <pristup2040@gmail.com>
2024-02-16 18:02:57 +1000

    add SimpleBot

---
+++ bot/src/main/java/edu/java/bot/SimpleBot.java
@@ -0,0 +1,49 @@
+package edu.java.bot;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import edu.java.bot.handlers.MainHandler;
+
+public class SimpleBot {
+    private final TelegramBot bot;
+    private final MainHandler commandHandler = new MainHandler();
+
+    public SimpleBot(String botToken) {
+        bot = new TelegramBot(botToken);
+    }
+
+    public Long getChaiId(Update update) {
+        return update.message().chat().id();
+    }
+
+    public String getMessageText(Update update) {
+        return update.message().text();
+    }
+
+    public void sendMessage(Long chatId, String text) {
+        SendMessage sendMessage = new SendMessage(chatId, text);
+        bot.execute(sendMessage);
+    }
+
+    private void processUpdate(Update update) {
+        commandHandler.handleCommand(update);
+    }
+
+    public void start() {
+        bot.setUpdatesListener(updates -> {
+            updates.forEach(this::processUpdate);
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        });
+    }
+
+    public static void main(String[] args) {
+        String botToken =  System.getenv("BOT_TOKEN");
+
+        SimpleBot bot = new SimpleBot(botToken);
+        bot.start();
+        System.out.println("boot_started");
+    }
+}
+
