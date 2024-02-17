a2d7a7e42eb7ef9002992701e359cfcc94350da4
Sergey <pristup2040@gmail.com>
2024-02-17 21:23:53 +1000

    spring bot!

--- bot/src/main/java/edu/java/bot/SimpleBot.java
+++ bot/src/main/java/edu/java/bot/SimpleBot.java
@@ -11,16 +11,21 @@ import edu.java.bot.handlers.MainHandler;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Log4j2
 @SuppressWarnings("all")
+@Component
 public class SimpleBot {
     private final TelegramBot bot;
     private final MainHandler commandHandler = new MainHandler();
 
-    public SimpleBot(String botToken) {
+    public SimpleBot(@Value("${BOT_TOKEN}") String botToken) {
         bot = new TelegramBot(botToken);
         setBotCommands();
+        start();
+        log.info("BOT started!");
     }
 
     public Long getChaiId(Update update) {
@@ -58,12 +63,5 @@ public class SimpleBot {
         });
     }
 
-    public static void main(String[] args) {
-        String botToken =  System.getenv("BOT_TOKEN");
-
-        SimpleBot bot = new SimpleBot(botToken);
-        bot.start();
-        System.out.println("boot_started");
-    }
 }
 
