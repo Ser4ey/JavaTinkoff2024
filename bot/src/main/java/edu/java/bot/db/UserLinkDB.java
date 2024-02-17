2da10676dfa4ab446496f2f9b3fd3aaa74fad0a7
Sergey <pristup2040@gmail.com>
2024-02-17 13:13:09 +1000

    add db

---
+++ bot/src/main/java/edu/java/bot/db/UserLinkDB.java
@@ -0,0 +1,11 @@
+package edu.java.bot.db;
+
+import java.util.List;
+
+public interface UserLinkDB {
+    List<String> getUserLinks(long userId);
+
+    void addUserLinks(long userId, String link);
+
+    void delUserLinks(long userId, String link);
+}
