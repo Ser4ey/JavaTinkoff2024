af43e6881dd61de13ff8f9d8832c4e0842b5a220
Sergey <pristup2040@gmail.com>
2024-02-17 13:38:45 +1000

    finish TrackCommand.java (test url validation)

--- bot/src/main/java/edu/java/bot/commands/ListCommand.java
+++ bot/src/main/java/edu/java/bot/commands/ListCommand.java
@@ -2,7 +2,10 @@ package edu.java.bot.commands;
 
 import com.pengrad.telegrambot.model.Update;
 import edu.java.bot.SimpleBot;
+import edu.java.bot.db.LocalDBFactory;
+import edu.java.bot.db.UserLinkDB;
 import edu.java.bot.states.State;
+import java.util.List;
 
 public class ListCommand extends AbstractCommand {
     public ListCommand() {
@@ -12,6 +15,21 @@ public class ListCommand extends AbstractCommand {
     @Override
     public void execute(SimpleBot bot, State state, Update update) {
         Long chatId = bot.getChaiId(update);
-        bot.sendMessage(chatId, "command: list");
+        UserLinkDB db = LocalDBFactory.getInstance();
+
+        List<String> links = db.getUserLinks(chatId);
+        if (links.isEmpty()) {
+            bot.sendMessage(chatId, "Нет ссылок!");
+            return;
+        }
+
+        StringBuilder linksText = new StringBuilder();
+        for (String link : links) {
+            linksText.append(link);
+            linksText.append("\n\n");
+        }
+        bot.sendMessage(chatId, linksText.toString());
+
     }
+
 }
