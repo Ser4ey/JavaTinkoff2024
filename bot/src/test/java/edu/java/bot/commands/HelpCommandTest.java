be135659a52d807bb489f2b488321f6d03895468
Sergey <pristup2040@gmail.com>
2024-02-23 19:17:58 +1000

    fix HelpCommandTest.java

--- bot/src/test/java/edu/java/bot/commands/HelpCommandTest.java
+++ bot/src/test/java/edu/java/bot/commands/HelpCommandTest.java
@@ -1,24 +1,25 @@
 package edu.java.bot.commands;
 
-import com.pengrad.telegrambot.model.Update;
-import edu.java.bot.SimpleBot;
+import edu.java.bot.chatbot.ChatBotMessageInterface;
 import edu.java.bot.states.State;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 class HelpCommandTest {
     @Test
     void testExecute() {
-        SimpleBot bot = Mockito.mock(SimpleBot.class);
         State state = Mockito.mock(State.class);
-        Update update = Mockito.mock(Update.class);
+        ChatBotMessageInterface chatMessage = Mockito.mock(ChatBotMessageInterface.class);
 
         HelpCommand helpCommand = Mockito.spy(new HelpCommand());
 
-        helpCommand.execute(bot, state, update);
+        CommandAnswer commandAnswer = helpCommand.execute(chatMessage, state);
 
-        Mockito.verify(bot, Mockito.times(1)).sendMessage(anyLong(), anyString());
+        assertNotNull(commandAnswer);
+        assertNotNull(commandAnswer.getAnswerText());
+        assertFalse(commandAnswer.isWithPagePreview());
     }
 }
