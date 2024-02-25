f0dc3ea69ef3414a3689e57749d46d091c437de4
Sergey <pristup2040@gmail.com>
2024-02-25 19:25:57 +1000

    add Scheduler

--- scrapper/src/main/java/edu/java/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/scrapper/ScrapperApplication.java
@@ -4,6 +4,7 @@ import edu.java.scrapper.configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
---
+++ scrapper/src/main/java/edu/java/scrapper/schedulers/LinkUpdaterScheduler.java
@@ -0,0 +1,14 @@
+package edu.java.scrapper.schedulers;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class LinkUpdaterScheduler {
+    @Scheduled(fixedRateString ="${app.scheduler.interval}")
+    public void update() {
+        log.info("Scheduler by {}", "https://github.com/Ser4ey/");
+    }
+}
