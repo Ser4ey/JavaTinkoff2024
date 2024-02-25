53fc68519ce7e38aeb04a9b8aa39fe2ce6240c55
Sergey <pristup2040@gmail.com>
2024-02-25 19:33:24 +1000

    finish hw2

--- scrapper/src/main/java/edu/java/scrapper/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/ApplicationConfig.java
@@ -3,6 +3,7 @@ package edu.java.scrapper.configuration;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.annotation.Validated;
 
@@ -10,6 +11,7 @@ import org.springframework.validation.annotation.Validated;
 @EnableScheduling
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
+    @Bean
     @NotNull
     Scheduler scheduler
 ) {
--- scrapper/src/main/java/edu/java/scrapper/schedulers/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scrapper/schedulers/LinkUpdaterScheduler.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Component;
 @Log4j2
 @Component
 public class LinkUpdaterScheduler {
-    @Scheduled(fixedRateString ="${app.scheduler.interval}")
+//    @Scheduled(fixedRateString = "${app.scheduler.interval}")
+    @Scheduled(fixedRateString = "#{@scheduler.interval().toMillis()}")
     public void update() {
         log.info("Scheduler by {}", "https://github.com/Ser4ey/");
     }
