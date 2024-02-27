fac4e117d60a96bbd1d8099c91936f5cf6319d53
Sergey <pristup2040@gmail.com>
2024-02-27 15:35:56 +1000

    @ConditionalOnProperty to LinkUpdaterScheduler

--- scrapper/src/main/java/edu/java/scrapper/schedulers/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scrapper/schedulers/LinkUpdaterScheduler.java
@@ -1,13 +1,16 @@
 package edu.java.scrapper.schedulers;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
+@EnableScheduling
+@ConditionalOnProperty(prefix = "app.scheduler", name = "enable", havingValue = "true", matchIfMissing = true)
 public class LinkUpdaterScheduler {
-//    @Scheduled(fixedRateString = "${app.scheduler.interval}")
     @Scheduled(fixedRateString = "#{@scheduler.interval().toMillis()}")
     public void update() {
         log.info("Scheduler by {}", "https://github.com/Ser4ey/");
