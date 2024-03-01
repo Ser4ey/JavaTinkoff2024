1a0e1187f2e09f24775a53002c7664edbdee8d66
Sergey <pristup2040@gmail.com>
2024-03-01 22:09:56 +1000

    add first dto

---
+++ bot/src/main/java/edu/java/bot/model/dto/ApiErrorResponse.java
@@ -0,0 +1,15 @@
+package edu.java.bot.model.dto;
+
+import java.util.List;
+
+public record ApiErrorResponse(
+    String description,
+    String code,
+    String exceptionName,
+    String exceptionMessage,
+    List<String> stacktrace
+) {
+
+}
+
+
