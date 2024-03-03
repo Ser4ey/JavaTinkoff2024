b6778e1ed87ecc49d9abebc04d612e7484e466aa
Sergey <pristup2040@gmail.com>
2024-03-03 16:23:44 +1000

    update ExceptionController.java

--- scrapper/src/main/java/edu/java/scrapper/controller/ExceptionController.java
+++ scrapper/src/main/java/edu/java/scrapper/controller/ExceptionController.java
@@ -1,5 +1,6 @@
 package edu.java.scrapper.controller;
 
+import edu.java.scrapper.exception.CustomResponseException;
 import edu.java.scrapper.model.dto.ApiErrorResponse;
 import java.util.Arrays;
 import java.util.List;
@@ -7,9 +8,43 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @RestControllerAdvice
 public class ExceptionController {
+    @ExceptionHandler(CustomResponseException.class)
+    public ResponseEntity<ApiErrorResponse> handleCustomResponseException(CustomResponseException ex) {
+        List<String> stacktrace = Arrays.stream(ex.getStackTrace())
+            .map(StackTraceElement::toString)
+            .toList();
+
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(
+            ex.getDescription(),
+            String.valueOf(ex.getHttpStatus().value()),
+            ex.getClass().getName(),
+            ex.getExceptionMessage(),
+            stacktrace
+        );
+
+        return new ResponseEntity<>(apiErrorResponse, ex.getHttpStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<String> stacktrace = Arrays.stream(ex.getStackTrace())
+            .map(StackTraceElement::toString)
+            .toList();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ApiErrorResponse(
+                "Validation failed",
+                "400",
+                ex.getClass().getName(),
+                ex.getMessage(),
+                stacktrace)
+            );
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiErrorResponse> handleException(Exception ex) {
         List<String> stacktrace = Arrays.stream(ex.getStackTrace())
@@ -18,7 +53,7 @@ public class ExceptionController {
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(new ApiErrorResponse(
-                ex.getMessage(),
+                "Unhandled server error",
                 "500",
                 ex.getClass().getName(),
                 ex.getMessage(),
