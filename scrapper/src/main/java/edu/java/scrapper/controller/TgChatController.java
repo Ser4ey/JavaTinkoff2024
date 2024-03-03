ca889f793189b853bb1e2f98cd0cd28072587837
Sergey <pristup2040@gmail.com>
2024-03-03 17:46:04 +1000

    finish TgChatController.java

--- scrapper/src/main/java/edu/java/scrapper/controller/TgChatController.java
+++ scrapper/src/main/java/edu/java/scrapper/controller/TgChatController.java
@@ -1,7 +1,16 @@
 package edu.java.scrapper.controller;
 
+import edu.java.scrapper.exception.ResponseException404;
+import edu.java.scrapper.exception.ResponseException409;
+import edu.java.scrapper.model.dto.ApiErrorResponse;
 import edu.java.scrapper.model.dto.LinkResponse;
-import edu.java.scrapper.model.dto.ListLinksResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Min;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,18 +19,54 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Tag(name = "Telegram Chat", description = "Telegram Chat API")
 @RestController
 @RequestMapping("/tg-chat")
 public class TgChatController {
 
     @PostMapping("/{id}")
-    public ResponseEntity<ListLinksResponse> registerChat(@PathVariable Long id) {
+    @Operation(summary = "Register a chat", description = "Register a new chat with a user")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Chat registered successfully"),
+        @ApiResponse(responseCode = "400", description = "Invalid request parameters", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorResponse.class))
+        }),
+        @ApiResponse(responseCode = "409", description = "The chat is already registered", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorResponse.class))
+        })
+    })
+    public ResponseEntity<Void> registerChat(@PathVariable @Min(1) Long id) {
+        // временное решение. Вся логика будет в @Service
+        if (id == 1) {
+            throw new ResponseException409(
+                "The chat is already registered",
+                "You cannot register a chat 2 times in a row"
+            );
+        }
+
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<LinkResponse> deleteChat(@PathVariable Long id) {
+    @Operation(summary = "Delete a chat", description = "Delete registered chat")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Chat registered successfully"),
+        @ApiResponse(responseCode = "400", description = "Invalid request parameters", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorResponse.class))
+        }),
+        @ApiResponse(responseCode = "404", description = "The chat was not found", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorResponse.class))
+        })
+    })
+    public ResponseEntity<LinkResponse> deleteChat(@PathVariable @Min(1) Long id) {
+        // временное решение. Вся логика будет в @Service
+        if (id == 1) {
+            throw new ResponseException404(
+                "The chat was not found",
+                "You can't delete something that doesn't exist"
+            );
+        }
+
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
