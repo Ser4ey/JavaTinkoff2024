ff402bcf6248a82c143f007a9b6b2cc7323ec1de
Sergey <pristup2040@gmail.com>
2024-03-03 23:12:38 +1000

    finish BotWebClient

--- scrapper/src/main/java/edu/java/scrapper/client/BotWebClient.java
+++ scrapper/src/main/java/edu/java/scrapper/client/BotWebClient.java
@@ -1,11 +1,14 @@
 package edu.java.scrapper.client;
 
+import edu.java.scrapper.exception.CustomRequestException;
+import edu.java.scrapper.model.dto.ApiErrorResponse;
 import edu.java.scrapper.model.dto.LinkUpdateRequest;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import org.springframework.http.HttpStatusCode;
 
 @Log4j2
 public class BotWebClient implements BotClient {
@@ -20,28 +23,14 @@ public class BotWebClient implements BotClient {
 
     @Override
     public void sendUpdates(LinkUpdateRequest linkUpdateRequest) {
-        Mono<ClientResponse> responseMono = webClient.post()
+        webClient.post()
             .uri("/updates")
             .bodyValue(linkUpdateRequest)
-            .exchange();
-
-        ClientResponse response = responseMono.block();
-
-        if (response == null) {
-            log.error("Response is null!");
-            return;
-        }
-
-        int statusCode = response.statusCode().value();
-        String responseBody = response.bodyToMono(String.class).block();
-
-        if (statusCode != HttpStatus.OK.value()) {
-            log.error(
-                "Error when updates | Status code: {} | Response: {} ",
-                statusCode,
-                responseBody
-            );
-        }
-
+            .retrieve()
+            .onStatus(HttpStatus.BAD_REQUEST::equals, clientResponse -> clientResponse.bodyToMono(ApiErrorResponse.class)
+                .flatMap(apiErrorResponse -> Mono.error(new CustomRequestException(apiErrorResponse)))
+            )
+            .toBodilessEntity()
+            .block();
     }
 }
