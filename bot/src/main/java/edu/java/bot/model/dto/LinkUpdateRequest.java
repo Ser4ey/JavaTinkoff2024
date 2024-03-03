36d533dd86c24227feb7cb9140a3e01a8a1e4ae7
Sergey <pristup2040@gmail.com>
2024-03-03 16:20:04 +1000

    validate DTO

--- bot/src/main/java/edu/java/bot/model/dto/LinkUpdateRequest.java
+++ bot/src/main/java/edu/java/bot/model/dto/LinkUpdateRequest.java
@@ -1,11 +1,14 @@
 package edu.java.bot.model.dto;
 
+import jakarta.validation.constraints.NotNull;
 import java.net.URI;
 import java.util.List;
 
 public record LinkUpdateRequest(
     Long id,
+    @NotNull
     URI url,
+    @NotNull
     String description,
     List<Long> tgChatIds
 ) {
--- scrapper/src/main/java/edu/java/scrapper/model/dto/ListLinksResponse.java
+++ scrapper/src/main/java/edu/java/scrapper/model/dto/ListLinksResponse.java
@@ -1,8 +1,10 @@
 package edu.java.scrapper.model.dto;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public record ListLinksResponse(
+    @NotNull
     List<LinkResponse> links,
     Integer size
 ) {
