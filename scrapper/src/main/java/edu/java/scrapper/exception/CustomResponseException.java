84213daf07bab0e5c6daf40375049bb83a161a6a
Sergey <pristup2040@gmail.com>
2024-03-03 16:19:15 +1000

    add exceptions

---
+++ scrapper/src/main/java/edu/java/scrapper/exception/CustomResponseException.java
@@ -0,0 +1,14 @@
+package edu.java.scrapper.exception;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.http.HttpStatus;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public abstract class CustomResponseException extends RuntimeException {
+    private final HttpStatus httpStatus;
+    private final String description;
+    private final String exceptionMessage;
+}
---
+++ scrapper/src/main/java/edu/java/scrapper/exception/ResponseException400.java
@@ -0,0 +1,9 @@
+package edu.java.scrapper.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ResponseException400 extends CustomResponseException{
+    public ResponseException400(String description, String exceptionMessage) {
+        super(HttpStatus.BAD_REQUEST, description, exceptionMessage);
+    }
+}
