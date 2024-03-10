66713977e16f5509225c1a7e08e4ed8be50966b8
Sergey <pristup2040@gmail.com>
2024-03-10 19:12:01 +1000

    finish homework task_1 and task_2

---
+++ migrations/init_tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE chat
+(
+    id      INTEGER PRIMARY KEY,
+    chat_id BIGINT UNIQUE NOT NULL
+);
+
+CREATE TABLE link
+(
+    id          INTEGER PRIMARY KEY,
+    url         VARCHAR(255) UNIQUE NOT NULL,
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE chat_link
+(
+    chat_id INTEGER,
+    link_id INTEGER,
+    PRIMARY KEY (chat_id, link_id),
+    FOREIGN KEY (chat_id) REFERENCES chat (id),
+    FOREIGN KEY (link_id) REFERENCES link (id)
+);
