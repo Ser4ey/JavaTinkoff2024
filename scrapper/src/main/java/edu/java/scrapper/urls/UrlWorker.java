21461500535f727764963a3a8b8bcd8a01db80ec
Sergey <pristup2040@gmail.com>
2024-03-20 19:26:10 +1000

    start urls

---
+++ scrapper/src/main/java/edu/java/scrapper/urls/UrlWorker.java
@@ -0,0 +1,29 @@
+package edu.java.scrapper.urls;
+
+import java.net.URI;
+import java.net.URL;
+
+public class UrlWorker {
+    private UrlWorker() {
+        throw new AssertionError("UrlWorker cannot be instantiated");
+    }
+
+    public static boolean isValidUrl(String urlString) {
+        try {
+            URL url = new URI(urlString).toURL();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static String getHostFromUrl(String urlString) {
+        try {
+            URL url = new URI(urlString).toURL();
+            return url.getHost();
+        } catch (Exception e) {
+            return null; // Если URL некорректен, вернуть null
+        }
+    }
+
+}
