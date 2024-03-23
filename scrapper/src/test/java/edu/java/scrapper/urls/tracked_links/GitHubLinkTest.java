29269a1ba3a8c2157dd20699c83a00b0e7c48211
Sergey <pristup2040@gmail.com>
2024-03-23 22:12:44 +1000

    update GitHubLinkTest

--- scrapper/src/test/java/edu/java/scrapper/urls/tracked_links/GitHubLinkTest.java
+++ scrapper/src/test/java/edu/java/scrapper/urls/tracked_links/GitHubLinkTest.java
@@ -5,19 +5,15 @@ import edu.java.scrapper.client.GitHubClient;
 import edu.java.scrapper.model.dto.GitHubOwnerRepoResponse;
 import java.net.URI;
 import java.time.OffsetDateTime;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class GitHubLinkTest extends IntegrationTest {
     @Mock
@@ -26,17 +22,13 @@ class GitHubLinkTest extends IntegrationTest {
     @InjectMocks
     private GitHubLink gitHubLink;
 
-    @BeforeAll
-    static void testSetUp() {
-    }
-
     @Test
     void testIsCurrentLinkHost() {
-        var gitHubURI = URI.create("https://github.com/Ser4ey/JavaTinkoff2024/pull/6");
-        var notGitHubURI = URI.create("https://notgithub.com/Ser4ey/JavaTinkoff2024/pull/6");
+        var goodURI = URI.create("https://github.com/Ser4ey/JavaTinkoff2024/pull/6");
+        var badURI = URI.create("https://notgithub.com/Ser4ey/JavaTinkoff2024/pull/6");
 
-        assertTrue(gitHubLink.isCurrentLinkHost(gitHubURI));
-        assertFalse(gitHubLink.isCurrentLinkHost(notGitHubURI));
+        assertTrue(gitHubLink.isCurrentLinkHost(goodURI));
+        assertFalse(gitHubLink.isCurrentLinkHost(badURI));
     }
 
     @Test
@@ -59,19 +51,19 @@ class GitHubLinkTest extends IntegrationTest {
             .getRepository("Ser4ey", "NotJavaTinkoff2024");
 
 
-        var GoodURI = URI.create("https://github.com/Ser4ey/JavaTinkoff2024/pull/6");
-        var BadURI = URI.create("https://github.com/Ser4ey/NotJavaTinkoff2024/pull/6");
-        var VeryBadURI = URI.create("1");
+        var goodURI = URI.create("https://github.com/Ser4ey/JavaTinkoff2024/pull/6");
+        var badURI = URI.create("https://github.com/Ser4ey/NotJavaTinkoff2024/pull/6");
+        var veryBadURI = URI.create("1");
 
-        assertTrue(gitHubLink.isWorkingUrl(GoodURI));
-        assertFalse(gitHubLink.isWorkingUrl(BadURI));
-        assertFalse(gitHubLink.isWorkingUrl(VeryBadURI));
+        assertTrue(gitHubLink.isWorkingUrl(goodURI));
+        assertFalse(gitHubLink.isWorkingUrl(badURI));
+        assertFalse(gitHubLink.isWorkingUrl(veryBadURI));
     }
 
     @Test
     void testGetLastActivityTime() {
         OffsetDateTime now = OffsetDateTime.now();
-        GitHubOwnerRepoResponse gitHubOwnerRepoResponse = new GitHubOwnerRepoResponse(
+        var gitHubOwnerRepoResponse = new GitHubOwnerRepoResponse(
             10L,
             "TinkoffJava2024",
             OffsetDateTime.MIN,
@@ -88,13 +80,13 @@ class GitHubLinkTest extends IntegrationTest {
             .getRepository("Ser4ey", "NotJavaTinkoff2024");
 
 
-        var GoodURI = URI.create("https://github.com/Ser4ey/JavaTinkoff2024/pull/6");
-        var BadURI = URI.create("https://github.com/Ser4ey/NotJavaTinkoff2024/pull/6");
+        var goodURI = URI.create("https://github.com/Ser4ey/JavaTinkoff2024/pull/6");
+        var badURI = URI.create("https://github.com/Ser4ey/NotJavaTinkoff2024/pull/6");
 
-        var time = gitHubLink.getLastActivityTime(GoodURI);
+        var time = gitHubLink.getLastActivityTime(goodURI);
         assertEquals(time.get(), now);
 
-        var time2 = gitHubLink.getLastActivityTime(BadURI);
+        var time2 = gitHubLink.getLastActivityTime(badURI);
         assertTrue(time2.isEmpty());
     }
 }
