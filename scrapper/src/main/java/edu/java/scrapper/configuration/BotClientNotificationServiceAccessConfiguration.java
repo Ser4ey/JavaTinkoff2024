d5ef3c0958e7bb9650cd46b0a8ba965b493ae0d6
Sergey <pristup2040@gmail.com>
2024-04-08 22:31:33 +1000

    NotificationServiceConfiguration -> BotClientNotificationServiceAccessConfiguration and KafkaNotificationServiceAccessConfiguration

--- scrapper/src/main/java/edu/java/scrapper/configuration/BotClientNotificationServiceAccessConfiguration.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/BotClientNotificationServiceAccessConfiguration.java
@@ -1,35 +1,26 @@
 package edu.java.scrapper.configuration;
 
 import edu.java.scrapper.client.BotClient;
-import edu.java.scrapper.kafka.KafkaQueueProducer;
 import edu.java.scrapper.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 @Log4j2
-public class NotificationServiceConfiguration {
-    private final ApplicationConfig applicationConfig;
+@ConditionalOnProperty(prefix = "app", name = "use-queue", havingValue = "false")
+public class BotClientNotificationServiceAccessConfiguration {
 
-    private final KafkaQueueProducer kafkaQueueProducer;
     private final BotClient botClient;
 
     @Bean
     public NotificationService notificationService() {
-        if (applicationConfig.useQueue()) {
-            return update -> {
-                // return kafkaQueueProducer::send;
-                log.debug("Используем Kafaka для отправки LinkUpdateRequest");
-                kafkaQueueProducer.send(update);
-            };
-        }
         return update -> {
             log.debug("Используем BotClient для отправки LinkUpdateRequest");
             botClient.sendUpdates(update);
         };
     }
-
 }
